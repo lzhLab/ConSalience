911aabc84d4fa054cebc502289f6adbc55d819ba
lzhLab <31695369+lzhLab@users.noreply.github.com>
2025-08-21 08:38:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ import argparse
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-from networks.unet3d_with_salience import UNet_3D
+from networks.unet3d_with_salience import UNet_3D_withSalience
+from networks.unet3d import UNet_3D
+from networks.AttenUNet_with_salience import AttenUNet_WithSalience
+from networks.AttenUNet import AttenUNet
+from networks.UNeXt_with_salience import UNeXt_WithSalience
+from networks.UNeXt import UNeXt
+from networks.unet_with_salience import UNet_WithSalience
+from networks.unet import UNet
+from networks.TransUNet_with_salience import TransUNet_WithSalience
+from networks.TransUNet import TransUNet
+from networks.YNet_with_salience import YNet_WithSalience
+from networks.YNet import YNet
+from networks.unetPlusPlus_with_salience import UNetPlusPlus_WithSalience
+from networks.unetPlusPlus import UNetPlusPlus
 from trainer import liver_vessel_seg
 
 
@@ -13,6 +26,7 @@ parser = argparse.ArgumentParser(description="Train the net on images and target
 parser.add_argument("--dataset", type=str, default="3Dircadb1")
 parser.add_argument("--img_size", type=tuple, default=(96,96,96))
 parser.add_argument("--method", type=str, default="unet_3d")
+parser.add_argument("--withSalience", type=int, default=1)
 parser.add_argument("--in_channels", type=int, default=3)
 parser.add_argument("--out_channels", type=int, default=1)
 parser.add_argument("--max_epochs", "-epo", type=int, default=1000)
@@ -58,10 +72,28 @@ if __name__ == '__main__':
     snapshot_path = snapshot_path + args.experiment
     if not os.path.exists(snapshot_path):
         os.makedirs(snapshot_path)
-    
-    model_config = {
-        "unet_3d": UNet_3D(args.in_channels, args.out_channels).cuda(), 
-    }
-    model = model_config[args.method]
 
+    if args.withSalience:
+        model_config = {
+            "unet_3d": UNet_3D_withSalience(args.in_channels, args.out_channels).cuda(),
+            "AttenUNet_with_salience": AttenUNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "UNeXt_with_salience": UNeXtWithSalience(args.in_channels, args.out_channels, args.img_size[0]).cuda(),
+            "unet_with_salience": UNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "TransUNet_with_salience": TransUNetWithSalience(args.in_channels, args.out_channels, args.img_size[0]).cuda(),
+            "YNet_with_salience": YNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "unetPlusPlus_with_salience": UNetPlusPlusWithSalience(args.in_channels, args.out_channels).cuda(),
+        }
+        model = model_config[args.method]
+    else:
+        model_config = {
+            "unet_3d": UNet_3D(args.in_channels, args.out_channels).cuda(),
+            "AttenUNet": AttenUNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "UNeXt": UNeXtWithSalience(args.in_channels, args.out_channels, args.img_size[0]).cuda(),
+            "unet": UNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "TransUNet": TransUNetWithSalience(args.in_channels, args.out_channels, args.img_size[0]).cuda(),
+            "YNet": YNetWithSalience(args.in_channels, args.out_channels).cuda(),
+            "unetPlusPlus": UNetPlusPlusWithSalience(args.in_channels, args.out_channels).cuda(),
+        }
+        model = model_config[args.method]
+    
     liver_vessel_seg(args, model, snapshot_path)
