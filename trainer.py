4c7e56364a76f5d83ff0c35fafbaa2f3ecae4260
lzhLab <31695369+lzhLab@users.noreply.github.com>
2025-08-21 08:39:58 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn
 import numpy as np 
 import pandas as pd
+import csv
+
+from scipy import stats
 
 from rich.progress import (
     BarColumn,
@@ -17,12 +20,14 @@ from rich.progress import (
 from loguru import logger
 from tensorboardX import SummaryWriter
 from torchvision import transforms
-from torch.data import DataLoader
-from .utils import test_single_volume
+from torch.utils.data import DataLoader
+from utils import test_single_volume
 
 
 def liver_vessel_seg(args, model, snapshot_path):
-    from datasets.liver_vessel import LiverVesselVolume, RandomGenerator, ToTensor, RandomCrop3D
+    import sys
+    sys.path.append('./datasets')
+    from liver_vessel import LiverVesselVolume, RandomGenerator, ToTensor, RandomCrop3D
     logger.add(
         snapshot_path + "/log.txt",
         format="{time:YYYY-MM-DD HH:mm:ss} {level} {message}",
@@ -35,7 +40,6 @@ def liver_vessel_seg(args, model, snapshot_path):
     base_lr = args.base_lr
     dataset = args.dataset
     img_size = args.img_size
-
 
     # Create database
     root_dir = f"./data/{dataset}/data_h5"
@@ -81,9 +85,9 @@ def liver_vessel_seg(args, model, snapshot_path):
         TextColumn("[progress.percentage]{task.percentage:>3.1f}%"),
         BarColumn(),
         MofNCompleteColumn(),
-        TextColumn("•"),
+        TextColumn("鈥?"),
         TimeElapsedColumn(),
-        TextColumn("•"),
+        TextColumn("鈥?"),
         TimeRemainingColumn(),
     )
     task = progress_bar.add_task(description="training", total=max_iterations)
@@ -110,7 +114,6 @@ def liver_vessel_seg(args, model, snapshot_path):
 
             iter_num += 1
             epoch_loss += loss.item()
-            print("loss: {:.4f}".format(loss.item()))
 
             lr_ = base_lr * (1.0 - iter_num / max_iterations) ** 0.9
             for param_group in optimizer.param_groups:
@@ -154,20 +157,52 @@ def liver_vessel_seg(args, model, snapshot_path):
     ########################################### Testing ################################################
     print("testing...")
     model.load_state_dict(torch.load(save_model_path))
-    metrics = 0.0
+    print("Model loaded!")
+
+    #The index structure of each sample is: {'dice': (mean, ci), 'acc': (mean, ci), ...} 
+    all_metrics_list = []
     for test_batch in test_loader:
         image_vol = test_batch['image'].cuda()
         label_vol = test_batch['label'].cuda()
         metric, _ = test_single_volume(model, image_vol, label_vol, "test", img_size)
-        metrics += np.array(metric)
-    metrics = np.array(metrics) / len(test_loader)
-    logger.info("="*25 + "Testing Result" + "="*25)
-    logger.info(
-        "Dice:{:.2f} clDice:{:.2f} Acc:{:.2f} Sen:{:.2f} Spe:{:.2f} HD95:{:.2f} ASSD:{:.2f}".format(
-            *metrics
-        )
-    )
-    writer.close()
+        all_metrics_list.append(metric)
+ 
+    ## Convert to a DataFrame, where each row is a sample and each column is a metric
+    headers = all_metrics_list[0].keys()
+    filename = snapshot_path + "/Per_sample_metrics.csv"
+    try:
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            writer.writerows(all_metrics_list)
+        print(f"Per-sample metrics saved to {filename}")
+    except IOError as e:
+        print(f"Failed to save per-sample metrics to {filename}")
+
+
+    # Aggregation index
+    final_metrics = {}
+    metric_names = list(all_metrics_list[0].keys()) # such as: dice, acc...
+    for metric in metric_names:
+        means = [sample[metric][0] for sample in all_metrics_list]
+        cis = [sample[metric][1] for sample in all_metrics_list]
+        print("means----",means,"cis-------",cis)
+        mean_avg = np.mean(means)
+        ci_avg = np.mean(cis)
+        final_metrics[metric] = (mean_avg, ci_avg)
+
+    logger.info("=" * 25 + " Final Metrics (95% CI)" + "=" * 25)
+    for k, v in final_metrics.items():
+            logger.info(f"{k}: {v[0]:.2f} 卤 {v[1]:.2f}")
+#    writer.close()
+
+    # Save as CSV file
+    csv_path = snapshot_path + "/final_test_metrics.csv"
+    df = pd.DataFrame([
+        {'Metric': k, 'Mean': v[0], '95% CI': v[1]}
+        for k, v in final_metrics.items()
+    ])
+    df.to_csv(csv_path, index=False)
+    print(f"Results saved to {csv_path}")
+
     return
-
-
